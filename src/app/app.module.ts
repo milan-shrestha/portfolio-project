2b29ed4d22b68a2cd8380aba9e9753796e5d1eb9
Milan Shrestha <milanshrestha17@hotmail.com>
2021-03-01 14:43:01 -0500

    Generated about component for the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule
