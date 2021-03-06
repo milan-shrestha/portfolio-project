b0d914a43f01b8df8fdc88e842e59a642067bae8
Milan Shrestha <milanshrestha17@hotmail.com>
2021-03-06 15:02:45 -0500

    Modified skills component

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -7,10 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SkillsComponent implements OnInit {
 
-  languages: string[] = ['javascript.png', 'java.png', 'html5.png', 'css-3.png']
+  languages: string[] = ['javascript.png', 'java.png', 'html5.png', 'css3.png']
   frameworks: string[] = ['spring-boot.png', 'thymeleaf.png', 'angular.png', 'bootstrap.png']
   databases: string[] = ['mysql.png']
-  tools: string[] = ['git.png', 'github.png', 'vsc.png', 'intelliJ.png']
+  tools: string[] = ['git.png', 'github.png', 'visual-studio-code.png', 'intelliJ.png']
 
   constructor() { }
 
