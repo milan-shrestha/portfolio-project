40b750422ad0ff6a37d09d197728ef039688f6d8
Milan Shrestha <milanshrestha17@hotmail.com>
2021-03-07 22:50:07 -0500

    Imported FormBuilder to contact.component.ts and used for validation of form fields

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
-  constructor() { }
+  FormData: FormGroup;
 
-  ngOnInit(): void {
+  constructor(private builder: FormBuilder) { }
+
+  ngOnInit() {
+
+    this.FormData = this.builder.group({
+      Fullname: new FormControl('', [Validators.required]),
+      Email: new FormControl('', [Validators.compose([Validators.required, Validators.email])]),
+      Message: new FormControl('', [Validators.required])
+    })
   }
 
 }
