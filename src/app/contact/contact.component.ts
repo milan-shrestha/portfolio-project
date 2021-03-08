8f33068ffe9391c808e13460a97c65d20d245f48
Milan Shrestha <milanshrestha17@hotmail.com>
2021-03-07 23:45:28 -0500

    Added onSubmit function to contact-component.ts to inject service to it

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ContactService } from '../contact.service';
 
 @Component({
   selector: 'app-contact',
@@ -10,7 +11,7 @@ export class ContactComponent implements OnInit {
 
   FormData: FormGroup;
 
-  constructor(private builder: FormBuilder) { }
+  constructor(private builder: FormBuilder, private contact: ContactService) { }
 
   ngOnInit() {
 
@@ -18,6 +19,17 @@ export class ContactComponent implements OnInit {
       Fullname: new FormControl('', [Validators.required]),
       Email: new FormControl('', [Validators.compose([Validators.required, Validators.email])]),
       Message: new FormControl('', [Validators.required])
+    })
+  }
+
+  onSubmit(FormData){
+    console.log(FormData);
+    this.contact.PostMessage(FormData).subscribe(response => {
+      location.href = 'https:/milan-shrestha.com/confirm';
+      console.log(response);
+    }, error => {
+      console.warn(error.responseText);
+      console.log({ error })
     })
   }
 
