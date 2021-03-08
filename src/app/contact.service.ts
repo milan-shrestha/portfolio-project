b6e3b382dc10e160ad01dcf69884c52cd555074a
Milan Shrestha <milanshrestha17@hotmail.com>
2021-03-07 23:32:51 -0500

    Added PostMessage method to contact.service

--- src/app/contact.service.ts
+++ src/app/contact.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,22 @@ import { Injectable } from '@angular/core';
 })
 export class ContactService {
 
-  constructor() { }
+  private api = 'https://formsubmit.co/65401d59935c2baacc61b10aca3bcfc8';
+
+  constructor(private http: HttpClient) { }
+
+  PostMessage(input: any) {
+    return this.http.post(this.api, input, { responseType: 'text' }).pipe(
+      map(
+        (response) => {
+          if(response) {
+            return response;
+          }
+        },
+        (error: any) => {
+          return error;
+        }
+      )
+    )
+  }
 }
