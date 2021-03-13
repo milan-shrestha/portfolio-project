4cdcdac304a9d6fa400a98d2b2cf557f37546bd7
Milan Shrestha <milanshrestha17@hotmail.com>
2021-03-13 15:31:31 -0500

    Added contents to projects section

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -7,6 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectsComponent implements OnInit {
 
+  projects: object[] = [{
+                         'img': 'piri.png',
+                         'title': 'Piri - Find Your Place',
+                         'description': 'This was my group project when I was learning to code. We used Java MVC architecture'+ 
+                                        ' to build .',
+                         'technologies': ['Java', 'Spring Boot', 'Thymeleaf', 'Hibernate', 'MySQL', 'JavaScript','Bootstrap', 'HTML', 'CSS'],
+                         'github-link': 'https://github.com/LaunchCodeLiftoffProjects/Piri/tree/working-develop',
+                         'live-link': ''              
+                        },
+                        {
+                          'img': 'gorkha-removals.png',
+                          'title': 'Gorkha Removals - A Moving Company',
+                          'description': 'It is a single page webpage.',
+                          'technologies': ['JavaScript', 'Bootstrap', 'HTML', 'CSS'],
+                          'github-link': 'https://github.com/milan-shrestha/gorkha-removals/tree/develop',
+                          'live-link': 'https://milan-shrestha.github.io/gorkha-removals/'
+                        }
+                      ]
+
   constructor() { }
 
   ngOnInit(): void {
